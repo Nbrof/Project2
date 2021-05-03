adb9227854d8a808c644a6b9475554bd81451880
Nikolaus Brodfuehrer <nbrof@Nikolauss-MacBook-Air.local>
2021-05-03 10:20:05 -0400

    Added Reactstrap CSS

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import { Jumbotron, Container } from 'reactstrap';
 
 const Main = (props) => {
 
@@ -35,8 +36,16 @@ const loaded = () => {
   console.log("Comictest", comic)
 
   return (
+    <div>
 
-    <h1>Hello</h1>
+    <Jumbotron fluid>
+      <Container fluid>
+        <h1 className="display-3">Fluid jumbotron</h1>
+        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </Container>
+    </Jumbotron>
+  </div>
+
   )
 }
 
